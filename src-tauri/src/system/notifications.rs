5e0ef41f4baf0c1f6a78bbf67b67a6da992cbbc3
JackpotMachine777 <jackpotmachinedev777@gmail.com>
2026-06-03 22:17:33 +0200

    Fixed notifications for Windows

--- src-tauri/src/system/notifications.rs
+++ src-tauri/src/system/notifications.rs
@@ -3,7 +3,15 @@ use notify_rust::{Notification, Urgency};
 #[tauri::command]
 pub fn send_notification(title: &str, message: &str, urgent: bool) -> Result<(), String> {
     let urgency = if urgent { Urgency::Critical } else { Urgency::Normal };
+    
+    #[cfg(target_os = "windows")]
+    Notification::new()
+        .summary(title)
+        .body(message)
+        .show()
+        .map_err(|e| format!("An error occured while sending notification: {}", e))?;
 
+    #[cfg(not(target_os = "windows"))]
     Notification::new()
         .summary(title)
         .body(message)
